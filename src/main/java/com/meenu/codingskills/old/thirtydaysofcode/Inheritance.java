d54abb220381b4e2a817e81636e209581648a0cf
Nair <meenu.nair@sap.com>
2020-04-11 15:38:59 +0530

    adding new files

---
+++ src/main/java/com/meenu/codingskills/old/thirtydaysofcode/Inheritance.java
@@ -0,0 +1,111 @@
+package com.meenu.codingskills.old.thirtydaysofcode;
+
+import java.util.Scanner;
+
+/*
+ * 
+ * Objective
+Today, we're delving into Inheritance. Check out the attached tutorial for learning materials and an instructional video!
+
+Task
+You are given two classes, Person and Student, where Person is the base class and Student is the derived class. Completed code for Person and a declaration for Student are provided for you in the editor. Observe that Student inherits all the properties of Person.
+
+Complete the Student class by writing the following:
+
+A Student class constructor, which has  parameters:
+A string, .
+A string, .
+An integer, .
+An integer array (or vector) of test scores, .
+A char calculate() method that calculates a Student object's average and returns the grade character representative of their calculated average:
+Grading.png
+
+Input Format
+
+The locked stub code in your editor calls your Student class constructor and passes it the necessary arguments. It also calls the calculate method (which takes no arguments).
+
+You are not responsible for reading the following input from stdin:
+The first line contains , , and , respectively. The second line contains the number of test scores. The third line of space-separated integers describes .
+
+Constraints
+
+Output Format
+
+This is handled by the locked stub code in your editor. Your output will be correct if your Student class constructor and calculate() method are properly implemented.
+ */
+
+public class Inheritance {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate());
+	}
+
+}
+
+class Student extends Person{
+	private int[] testScores;
+    
+    public Student(String firstName, String lastName, int id, int[] scores){
+        super(firstName, lastName, id);
+        this.testScores = scores;
+    }
+
+    public char calculate(){
+        int count = testScores.length;
+        int sum = 0;
+        for(int i = 0; i < count; i++){
+            sum = sum + testScores[i];
+        }
+        int avg = sum / count;
+        if(avg >= 90 && avg <= 100){
+            return 'O';
+        }
+        if(avg >= 80 && avg < 90){
+            return 'E';
+        }
+        if(avg >= 70 && avg < 80){
+            return 'A';
+        }
+        if(avg >= 55 && avg < 70){
+            return 'P';
+        }
+        if(avg >= 40 && avg < 55){
+            return 'D';
+        }
+        else
+            return 'T';   
+    }
+}
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
