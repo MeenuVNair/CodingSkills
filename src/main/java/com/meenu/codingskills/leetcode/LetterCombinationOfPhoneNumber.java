e5f7278bf47ac015114564072992da199184c583
Nair <meenu.nair@sap.com>
2020-04-11 20:01:44 +0530

    added few leetcode programs

---
+++ src/main/java/com/meenu/codingskills/leetcode/LetterCombinationOfPhoneNumber.java
@@ -0,0 +1,65 @@
+package com.meenu.codingskills.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Problem description: Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent.
+
+A mapping of digit to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+
+Example:
+Input: "23"
+Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ */
+
+public class LetterCombinationOfPhoneNumber {
+	@SuppressWarnings("serial")
+	Map<String , String> phone = new HashMap<String, String>(){{
+											put("2", "abc");
+											put("3", "def");
+											put("4", "ghi");
+											put("5", "jkl");
+											put("6", "mno");
+											put("7", "pqrs");
+											put("8", "tuv");
+											put("9", "wxyz"); 
+											}};
+	
+	static List<String> combinationList = new ArrayList<String>();
+
+	public static void main(String[] args) {
+		
+		LetterCombinationOfPhoneNumber obj = new LetterCombinationOfPhoneNumber();
+		String input = "23";
+		obj.letterCombination(input);
+		
+		System.out.println("combinations : ");
+		for(int i = 0; i < combinationList.size(); i++) {
+			System.out.print(combinationList.get(i) + " ");
+		}
+	}
+	
+	public void backtrack(String combination, String next_digits) {
+		if(next_digits.length() == 0) {
+			combinationList.add(combination);
+		}
+		else {
+			String digit = next_digits.substring(0,1);
+			String letters = phone.get(digit);
+			for(int i = 0; i < letters.length() ; i++) {
+				String letter = phone.get(digit).substring(i, i+1);
+				backtrack(combination+letter, next_digits.substring(1));
+			}
+		}
+	}
+
+	@SuppressWarnings("null")
+	public List<String> letterCombination(String digits){
+		if(digits != null || digits.length() != 0)
+			backtrack("", digits);
+		return combinationList;
+	}
+}
---
+++ src/main/java/com/meenu/codingskills/leetcode/SingleNumberII.java
@@ -0,0 +1,49 @@
+package com.meenu.codingskills.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * Problem description: Given a non-empty array of integers, every element appears three times except for one, which appears exactly once. Find that single one.
+
+Note: Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+Input: [2,2,3,2]
+Output: 3
+
+Example 2:
+Input: [0,1,0,1,0,1,99]
+Output: 99
+ 
+ */
+
+public class SingleNumberII {
+	Map<Integer,Integer> map;
+
+	public static void main(String[] args) {
+		SingleNumberII obj = new SingleNumberII();
+		int array[] = new int[] {0,1,0,1,0,1,99}; // {2,2,3,2}; 
+		obj.map = new HashMap<Integer,Integer>();
+		int singleNumber = obj.findSingleNumber(array);
+		if(singleNumber == -1)
+			System.out.println("No single number found");
+		else
+			System.out.println("Single number : " + singleNumber);
+	}
+
+	public int findSingleNumber(int[] array) {
+		for(int i = 0; i < array.length; i++) {
+			if(map.containsKey(array[i]))
+				map.put(array[i], map.get(array[i]) + 1);
+			else
+				map.put(array[i], 1);
+		}
+		
+		for(int i : map.keySet()) {
+			if(map.get(i) == 1)
+				return i;
+		}
+		return -1;
+	}
+}
